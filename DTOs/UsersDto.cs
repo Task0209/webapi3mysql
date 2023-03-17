cc888637e4cc3d617f88a9f11cf040bfba5b507a
Vinoth <dummypoint22@gmail.com>
2023-03-17 06:17:28 +0000

    First

---
+++ DTOs/UsersDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myapp.DTOs
+{
+    public class UsersDto
+    {
+        public string UserName { get; set; }
+        public string Hobbies { get; set; }
+        public string Location { get; set; }
+    }
+}
